2fcd39a3b13f79d49aa384da7d416bf967756804
Mj Alavi <41288915+mjalav@users.noreply.github.com>
2022-12-14 13:19:27 +0800

    Update Common.ts
    changed base url to use app-automate instred of automate

--- src/lib/Common.ts
+++ src/lib/Common.ts
@@ -20,7 +20,7 @@ export class Common {
     }
 
     async exposeBuildLink (sessionId:string, currentConfig:any, defaultBsAuth:any) {
-        const res = await api.makeGetRequest(`${bsEndpoint}/automate/sessions/${sessionId}.json`, { ...defaultBsAuth });
+        const res = await api.makeGetRequest(`${bsEndpoint}/app-automate/sessions/${sessionId}.json`, { ...defaultBsAuth });
 
         // eslint-disable-next-line max-len
         const exposedUrl = currentConfig.shortUrl ? this.shortenUrl(res.data.automation_session.public_url) : res.data.automation_session.public_url;
@@ -30,7 +30,7 @@ export class Common {
 
     async updateBuild (sessionId:string, data:any, currentConfig:any, defaultBsAuth:any) {
         if ((currentConfig.user && currentConfig.key) && (currentConfig.user !== '' && currentConfig.key !== '')) {
-            await api.makePutRequest(`${bsEndpoint}/automate/sessions/${sessionId}.json`, data, { ...defaultBsAuth });
+            await api.makePutRequest(`${bsEndpoint}/app-automate/sessions/${sessionId}.json`, data, { ...defaultBsAuth });
             await this.exposeBuildLink(sessionId, currentConfig, defaultBsAuth);
         } else {
             output.log('No Browserstack credentials found. Probably you are not running with Browserstack!');
